9bb5b9c276ec23ad30a520d8daa359d5332c20d9
tazkiajessy <jessy566eee@gmail.com>
2021-03-10 22:30:47 +0100

    CRUD method for db

--- index.js
+++ index.js
@@ -27,5 +27,40 @@ app.post("/users", async (req, res, next) => {
   }
 });
 //http -v POST :4001/users email=Peter@gmail.com
+//http -v POST :4001/users name="Peter" password=123 email=Peter@gmail.com
+
+//updating a user
+app.put("/users/:id", async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    const { phone } = req.body;
+    const userToUpdate = await User.findByPk(id);
+    if (!userToUpdate) {
+      res.status(404).send("User not found");
+    } else {
+      const updatedUser = await userToUpdate.update({ phone });
+      res.send(updatedUser);
+    }
+  } catch (error) {
+    next(error.message);
+  }
+});
+// http -v PUT :4001/users/4 phone=1234567
+
+//delete a user
+app.delete("/users/:id", async (req, res, next) => {
+  try {
+    const { id } = parseInt(req.params);
+    const userToDelete = await User.findByPk(id);
+    if (!userToDelete) {
+      res.status(404).send("User not found");
+    } else {
+      const deletedUser = await userToDelete.destroy();
+      res.send(deletedUser);
+    }
+  } catch (error) {
+    next(error.message);
+  }
+});
 
 app.listen(PORT, () => console.log(`Server started in port: ${PORT}`));
